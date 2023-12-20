62835173465ebb2fa650e2dfbbd2b902faccc342
Rick South <rick@natamus.com>
2023-12-20 16:06:54 +0100

    No Animal Tempt Delay source code for 1.19.2.

---
+++ Common/src/main/java/com/natamus/noanimaltemptdelay/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.noanimaltemptdelay.util;
+
+public class Reference {
+	public static final String MOD_ID = "noanimaltemptdelay";
+	public static final String NAME = "No Animal Tempt Delay";
+	public static final String VERSION = "1.0";
+	public static final String ACCEPTED_VERSIONS = "[1.19.2]";
+}
---
+++ Fabric/src/main/java/com/natamus/noanimaltemptdelay/fabric/mixin/TemptGoalMixin.java
@@ -0,0 +1,18 @@
+package com.natamus.noanimaltemptdelay.fabric.mixin;
+
+import net.minecraft.world.entity.ai.goal.TemptGoal;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(value = TemptGoal.class, priority = 1001)
+public class TemptGoalMixin {
+	@Shadow private int calmDown;
+
+	@Inject(method = "stop()V", at = @At(value = "TAIL"))
+	public void stop(CallbackInfo ci) {
+		this.calmDown = 0;
+	}
+}
