42d266943534cd53f5f0bca42adec6c01fd2291d
Rick South <rick@natamus.com>
2024-07-28 07:23:45 +0200

    (v1.2) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Common/src/main/java/com/natamus/noanimaltemptdelay/util/Reference.java
+++ Common/src/main/java/com/natamus/noanimaltemptdelay/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.noanimaltemptdelay.util;
 public class Reference {
 	public static final String MOD_ID = "noanimaltemptdelay";
 	public static final String NAME = "No Animal Tempt Delay";
-	public static final String VERSION = "1.1";
+	public static final String VERSION = "1.2";
 	public static final String ACCEPTED_VERSIONS = "[1.20.1]";
 }
